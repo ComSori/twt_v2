b314d6d694bfe2fbab8d57c4980e70f0161ce375
Jhsysng <ysng1118@naver.com>
2023-01-29 12:23:20 +0900

    project commit

---
+++ src/main/java/com/comsori/twt/TwtV2Application.java
@@ -0,0 +1,13 @@
+package com.comsori.twt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwtV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwtV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/comsori/twt/TwtV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comsori.twt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwtV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
