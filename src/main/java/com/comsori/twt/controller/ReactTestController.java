d4fe5e02899d82e5a90366c99d8f889d1f0cdf1a
Jhsysng <ysng1118@naver.com>
2023-02-16 15:10:41 +0900

    r-=s

--- src/main/java/com/comsori/twt/controller/ReactTestController.java
+++ src/main/java/com/comsori/twt/controller/ReactTestController.java
@@ -1,2 +1,13 @@
-package com.comsori.twt.controller;public class ReactTestController {
+package com.comsori.twt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReactTestController {
+
+    @GetMapping("/api/hello")
+    public String test(){
+        return "hello, react-spring";
+    }
 }
